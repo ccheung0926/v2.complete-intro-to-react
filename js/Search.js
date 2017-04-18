3e8ecddadc0d0e43b3d7e9aed03a594d47b3e5cb
ccheung0926 <cindycheung0926@gmail.com>
2017-04-18 01:34:41 -0700

    add filter function

--- js/Search.js
+++ js/Search.js
@@ -8,9 +8,9 @@ const Search = React.createClass({
       searchTerm: 'this is the default string'
     }
   },
-  handleSearchTermChange (event){
+  handleSearchTermChange (event) {
     // use this.setState all time
-    this.setState({searchTerm: event.target.value}) //<-- this.setState is an asynchronous functions that is going to schdule an update and eventually, it is going to empty that buffer into one setState, so only updating the bare minimum of things possible to keep your app performing
+    this.setState({searchTerm: event.target.value}) // <-- this.setState is an asynchronous functions that is going to schdule an update and eventually, it is going to empty that buffer into one setState, so only updating the bare minimum of things possible to keep your app performing
     // or this.state.searchTerm = event.target.value -- but this will cause the state and UI are not in sync, and the broswer does not re-render coz no one is watching
     // this.forceUpdate() -- update right now
   },
@@ -18,14 +18,18 @@ const Search = React.createClass({
     return (
       <div className='search'>
         <header>
-          <h1>{this.state.searchTerm}</h1>
+          <h1>svideo</h1>
           <input onChange={this.handleSearchTermChange} type='text' placeholder='Search' value={this.state.searchTerm} />
         </header>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} {...show} />
-          )
-        })}
+        {preload.shows
+          .filter((show) => {
+            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+          })
+          .map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
       </div>
     )
   }
